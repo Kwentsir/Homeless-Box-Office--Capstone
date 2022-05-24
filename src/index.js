273ce65eea6e7bc4792490bd29a369cb92f11deb
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-24 11:29:18 +0300

    Add api infos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './style.css';
+import "./style.css";
+
+// const involveapi = 'yVTwDpeZ7FtqX6HWOiZh';
+
+const fetchData = async () => {
+  await fetch("https://api.tvmaze.com/shows")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+fetchData();
