fffc7dd74ba20b83da17a1e8b3a445c8b61e545d
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-24 14:43:54 +0300

    Add get button id function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import './style.css';
+import "./style.css";
 
 // const involveapi = 'yVTwDpeZ7FtqX6HWOiZh';
 
 const fetchData = async () => {
-  await fetch('https://api.tvmaze.com/shows')
-    .then((response) => response.json());
+  await fetch("https://api.tvmaze.com/shows").then((response) =>
+    response.json()
+  );
   // .then((json) => alert(json));
 };
 
 fetchData();
+
+const commentButtons = document.querySelectorAll(".comment");
+
+let addClickEvent = (button) => {
+  button.addEventListener("click", getIdFromButton);
+};
+
+let getIdFromButton = (event) => {
+  let targetEvent = event.target;
+  return targetEvent.id;
+};
+
+commentButtons.forEach(addClickEvent);
