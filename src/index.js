cbb16a253f2fe649257368b239d04e87c6253f7d
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-24 22:37:05 +0300

    Add styling to the modal

--- src/index.js
+++ src/index.js
@@ -1,54 +1,43 @@
-import "./style.css";
+import './style.css';
 
 // const involveapi = 'yVTwDpeZ7FtqX6HWOiZh';
 
-// const fetchData = async () => {
-//   await fetch("https://api.tvmaze.com/shows").then((response) =>
-//     response.json()
-//   );
-//    .then((json) => alert(json));
-// };
-
-// fetchData();
-
 const getSingleMovieData = async (id) => {
-  let response = await fetch(`https://api.tvmaze.com/shows/${id}`);
-  let singleData = await response.json();
+  const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const singleData = await response.json();
   return singleData;
 };
 
-const commentButtons = document.querySelectorAll(".comment");
-
-const addClickEvent = (button) => {
-  button.addEventListener("click", openPopup);
-};
+const commentButtons = document.querySelectorAll('.comment');
 
 const openPopup = (event) => {
-  let targetId = event.target.id;
+  const targetId = event.target.id;
 
-  let dialog = document.querySelector('dialog');
+  const dialog = document.querySelector('dialog');
   dialog.showModal();
 
   getSingleMovieData(targetId).then((singleData) => {
-  
     dialog.innerHTML = `
     <a id="close-button" class="comment-popup-close-button" href="#">X</a>
-   
-    <div id="image-frame">
+   <div class="poster-container">
     <img id="media-poster" src="${singleData.image.original}">
     </div>
+<div class="movie-description">
     <p>${singleData.name} ${singleData.rating.average}</p>
     <p><strong>Genre:</strong> ${singleData.genres}</p>
     <p>${singleData.summary}</p>
+    </div>
     <br>
     `;
 
-
-    let closeButton = document.querySelector("#close-button");
-    closeButton.addEventListener("click", () => {
+    const closeButton = document.querySelector('#close-button');
+    closeButton.addEventListener('click', () => {
       dialog.close();
     });
   });
+};
+const addClickEvent = (button) => {
+  button.addEventListener('click', openPopup);
 };
 
 commentButtons.forEach(addClickEvent);
