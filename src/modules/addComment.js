6e320a696cde4ff9c2c9a764d07e432ed1626499
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-25 14:59:00 +0300

    Add styling to the comment form

--- src/modules/addComment.js
+++ src/modules/addComment.js
@@ -1,29 +1,29 @@
 const addComment = (itemId) => {
-  const urlRoot =
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yVTwDpeZ7FtqX6HWOiZh/comments";
-  let commentForm = document.querySelector("#add-comment");
-  commentForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let reviewerSelector = document.querySelector("#reviewer-name");
-    let reviewSelector = document.querySelector("#review");
-    let reviewer = reviewerSelector.value;
-    let review = reviewSelector.value;
-    postFunction(reviewer, review);
-  });
+  const urlRoot = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yVTwDpeZ7FtqX6HWOiZh/comments';
 
   const postFunction = (reviewer, review) => {
     fetch(`${urlRoot}`, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         item_id: `item${itemId}`,
         username: `${reviewer}`,
         comment: `${review}`,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     });
   };
+
+  const commentForm = document.querySelector('#add-comment');
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const reviewerSelector = document.querySelector('#reviewer-name');
+    const reviewSelector = document.querySelector('#review');
+    const reviewer = reviewerSelector.value;
+    const review = reviewSelector.value;
+    postFunction(reviewer, review);
+  });
 };
 
 export default addComment;
