b0b6d7d69a68d6149c1f7422ba3755fe35965209
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-25 12:26:34 +0300

    Add get and display comment function

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,3 +1,5 @@
+import getComment from "./getComment.js";
+
 const commentFunction = () => {
   const getSingleMovieData = async (id) => {
     const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
@@ -5,36 +7,41 @@ const commentFunction = () => {
     return singleData;
   };
 
-  const commentButtons = document.querySelectorAll('.comment');
+  const commentButtons = document.querySelectorAll(".comment");
 
   const openPopup = (event) => {
     const targetId = event.target.id;
 
-    const dialog = document.querySelector('dialog');
+    const dialog = document.querySelector("dialog");
     dialog.showModal();
 
     getSingleMovieData(targetId).then((singleData) => {
       dialog.innerHTML = `
-      <a id="close-button" class="comment-popup-close-button" href="#">X</a>
-     <div class="poster-container">
-      <img id="media-poster" src="${singleData.image.original}">
-      </div>
-  <div class="movie-description">
-      <p>${singleData.name} ${singleData.rating.average}</p>
-      <p><strong>Genre:</strong> ${singleData.genres}</p>
-      <p>${singleData.summary}</p>
-      </div>
-      <br>
+          <a id="close-button" class="comment-popup-close-button" href="#">X</a>
+        <div class="poster-container">
+          <img id="media-poster" src="${singleData.image.original}">
+          </div>
+      <div class="movie-description">
+          <p>${singleData.name} ${singleData.rating.average}</p>
+          <p><strong>Genre:</strong> ${singleData.genres}</p>
+          <p>${singleData.summary}</p>
+          </div>
+          <br>
+          <ul id="comment-section">
+          <h4>Reviews</h4>
+          </ul>
       `;
 
-      const closeButton = document.querySelector('#close-button');
-      closeButton.addEventListener('click', () => {
+      const closeButton = document.querySelector("#close-button");
+      closeButton.addEventListener("click", () => {
         dialog.close();
       });
+      //Get comment function
+      getComment(targetId);
     });
   };
   const addClickEvent = (button) => {
-    button.addEventListener('click', openPopup);
+    button.addEventListener("click", openPopup);
   };
 
   commentButtons.forEach(addClickEvent);
