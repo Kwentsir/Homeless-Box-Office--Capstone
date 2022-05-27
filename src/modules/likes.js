85a433a1034d6ff087a9ff9d5cd4735add1b4290
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-27 08:02:51 +0000

    update code

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,35 +1,18 @@
-const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yVTwDpeZ7FtqX6HWOiZh';
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WSivkFrNizGiZHAzjITk';
 const getLikes = async () => {
   const response = await fetch(`${baseUrl}/likes`);
   const data = await response.json();
   return data;
 };
 
-const postLike = async (movieId) => {
+const postLikes = async (movieId) => {
   const response = await fetch(`${baseUrl}/likes`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      item_id: movieId,
-    }),
+    body: JSON.stringify({ item_id: movieId }),
   });
-  response;
+  response();
 };
-
-const updateLikes = async (movieId, likes) => {
-  const data = {
-    item_id: movieId,
-    likes,
-  };
-  const response = await fetch(`${baseUrl}/likes`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  response;
-};
-export { getLikes, postLike, updateLikes };
+export { getLikes, postLikes };
--- src/modules/movieList.js
+++ src/modules/movieList.js
@@ -1,59 +1,65 @@
-import enableReserve from "./reserve.js";
-import commentFunction from "./comment.js";
-import { getLikes, postLike, updateLikes } from "./likes.js";
+import enableReserve from './reserve.js';
+import commentFunction from './comment.js';
+import { getLikes, postLikes } from './likes.js';
 
 export const listenHeartClicks = (movieId, likes) => {
-  const likeHeart = document.querySelectorAll(".fa-heart");
+  const likeHeart = document.querySelectorAll('.fa-heart');
   likeHeart.forEach((like) => {
     if (like.id === movieId) {
       likes = Number(likes) + 1;
       like.nextElementSibling.textContent = likes;
-      updateLikes(movieId, likes);
+      postLikes(movieId);
     }
   });
 };
 
-const displayMovies = async (data) => {
+const updateLikes = async () => {
   const allLikes = await getLikes();
-  const displayMovies = document.querySelector("#movie-list");
-  data.forEach((movie) => {
-    let movieLikes = 0;
-    allLikes.forEach((like) => {
-      if (like.item_id === movie.id) {
-        movieLikes = like.likes;
+  const heartSpans = document.querySelectorAll('.like-span');
+
+  heartSpans.forEach((span) => {
+    allLikes.forEach((likeObject) => {
+      if (span.id === likeObject.item_id) {
+        span.innerHTML = likeObject.likes;
       }
     });
-    displayMovies.innerHTML += `<article class="movie">
-<img src="${movie.image.medium}" alt="${movie.name}">
-<h2>${movie.name}</h2>
-<p class="genre">${movie.genres}</p>
-<div class="like-rating">
-<i class="fa-solid fa-heart" id="${movie.id}"></i>
-<span>${movieLikes}</span>
-<i class="fa-solid fa-star" id="${movie.id}"></i>
-</div>
-<div class="button">
-<button class="comment" id="${movie.id}">Comments</button>
-<button id="${movie.id}" class="show-reserve-popup" 
-banner="${movie.image.medium}" 
-title="${movie.name}" 
-language="${movie.language}"
-premier="${movie.premiered}"
-ended="${movie.ended}"
-rating="${movie.rating.average}">Reservations</button>
-</article>
-`;
   });
 };
 
+const displayMovies = async (data) => {
+  const displayMovies = document.querySelector('#movie-list');
+  data.forEach((movie) => {
+    displayMovies.innerHTML += `<article class="movie">
+    <img src="${movie.image.medium}" alt="${movie.name}">
+    <h2>${movie.name}</h2>
+    <p class="genre">${movie.genres}</p>
+    <div class="like-rating">
+    <i class="fa-solid fa-heart" id="${movie.id}"></i>
+    <span id="${movie.id}" class="like-span">0</span>
+    <i class="fa-solid fa-star" id="${movie.id}"></i>
+    </div>
+    <div class="button">
+    <button class="comment" id="${movie.id}">Comments</button>
+    <button id="${movie.id}" class="show-reserve-popup" 
+    banner="${movie.image.medium}" 
+    title="${movie.name}" 
+    language="${movie.language}"
+    premier="${movie.premiered}"
+    ended="${movie.ended}"
+    rating="${movie.rating.average}">Reservations</button>
+    </article>
+`;
+  });
+  updateLikes();
+};
+
 const getMovies = async () => {
-  const response = await fetch("https://api.tvmaze.com/shows");
+  const response = await fetch('https://api.tvmaze.com/shows');
   const data = await response.json();
   const slicedData = data.slice(0, 50);
-  slicedData.forEach((movie) => postLike(movie.id));
   displayMovies(slicedData);
   enableReserve();
   commentFunction();
 };
-getMovies();
+
 export default getMovies;
