b4e420ebc92d2f20cdeb19b1b723e02d393b98af
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-05-27 19:26:50 +0200

    fixed linters

--- resev test/addReservation-test.js
+++ resev test/addReservation-test.js
@@ -1,11 +1,11 @@
 /**
  * @jest-environment jsdom
  */
- import addReservation from './addReservation.js';
+import addReservation from './addReservation.js';
 
- test('count the number of the li elements in the review section', () => {
-   // arrange
-   document.body.innerHTML = `
+test('count the number of the li elements in the review section', () => {
+  // arrange
+  document.body.innerHTML = `
      <ul id="reserve-section">
      <li>1</li>
      <li>2</li>
@@ -13,9 +13,8 @@
      <li>4</li>
      </ul>
    `;
-   // act
-   expect(addReservation())
-   // assert
-     .toBe(4);
- });
- 
+  // act
+  expect(addReservation())
+  // assert
+    .toBe(4);
+});
