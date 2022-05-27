add3c57a4b388d64e1524190493950494cf9aa3d
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-27 12:48:53 +0000

    fix linter errors

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -1,5 +1,4 @@
-const baseUrl =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yVTwDpeZ7FtqX6HWOiZh";
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/yVTwDpeZ7FtqX6HWOiZh';
 const getLikes = async () => {
   const response = await fetch(`${baseUrl}/likes`);
   const data = await response.json();
